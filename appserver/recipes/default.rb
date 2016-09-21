b54161f66c0dafb02ddc9a85e246d0b523c5d3e4
Will Button <will@willbutton.co>
2016-09-21 16:10:11 -0700

    explicitly declaring nodejs

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -6,8 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "nodejs"
 include_recipe "nodejs::nodejs_from_binary"
 # Or set a specific version of nodejs to be installed
 node.default['nodejs']['install_method'] = 'binary'
 node.default['nodejs']['version'] = '6.6.0'
-node.default['nodejs']['binary']['checksum'] = 'c22ab0dfa9d0b8d9de02ef7c0d860298a5d1bf6cae7413fb18b99e8a3d25648a' 
+node.default['nodejs']['binary']['checksum'] = 'c22ab0dfa9d0b8d9de02ef7c0d860298a5d1bf6cae7413fb18b99e8a3d25648a'
