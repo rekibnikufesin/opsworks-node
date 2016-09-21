ad257b9b6727460f46d9acb150b366f3cb7796a9
Will Button <will@willbutton.co>
2016-09-21 16:21:43 -0700

    Adding build-essential and removing unneeded dependencies

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -8,11 +8,7 @@ source_url 'https://github.com/redguide/nodejs' if respond_to?(:source_url)
 issues_url 'https://github.com/redguide/nodejs/issues' if respond_to?(:issues_url)
 version '2.4.5'
 
-depends 'yum-epel'
 depends 'build-essential'
-depends 'ark'
-depends 'apt', '>= 2.9.1'
-depends 'homebrew'
 
 %w(debian ubuntu centos redhat scientific oracle amazon smartos mac_os_x).each do |os|
   supports os
