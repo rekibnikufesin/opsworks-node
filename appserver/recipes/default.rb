55bb1a1a8ea9fc977de14d46aee8f020797d3d93
Will Button <will@willbutton.co>
2016-09-22 22:24:22 -0700

    Crap. That should all be in deploy, not setup

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -9,26 +9,3 @@ yum_package 'ImageMagick'
 execute 'install pm2' do
   command 'npm install pm2 -g'
 end
-
-instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
-layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
-
-if layers.include?("api-layer")
-    Chef::Log.info("** setting container to api")
-    execute 'set api env var' do
-      command 'export CONTAINER=api'
-    end
-elsif layers.include?("web-layer")
-    Chef::Log.info("** setting container to web")
-    execute 'set api env var' do
-      command 'export CONTAINER=web'
-    end
-else
-    Chef::Log.info("** setting container to unknown")
-    execute 'set api env var' do
-      command 'export CONTAINER=unknown'
-    end
-end
-execute 'check env' do
-  command 'echo $CONTAINER'
-end
