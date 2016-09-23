ebebee811a0f73e884bc99fb742fe58bdeccf29f
Will Button <will@willbutton.co>
2016-09-22 21:57:20 -0700

    more hijinx

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -16,16 +16,19 @@ layers = instance['role'] # the attribute formerly known as 'layers' via opswork
 if layers.include?("api-layer")
     Chef::Log.info("** setting container to api")
     execute 'set api env var' do
-      command 'export CONTAINER="api"'
+      command 'echo CONTAINER="api" > /etc/environment'
     end
 elsif layers.include?("web-layer")
     Chef::Log.info("** setting container to web")
     execute 'set api env var' do
-      command 'export CONTAINER="web"'
+      command 'echo CONTAINER="web" > /etc/environment'
     end
 else
     Chef::Log.info("** setting container to unknown")
     execute 'set api env var' do
-      command 'export CONTAINER="unknown"'
+      command 'echo CONTAINER="unknown" > /etc/environment'
     end
+end
+execute 'update env' do
+  command 'source /etc/environment'
 end
