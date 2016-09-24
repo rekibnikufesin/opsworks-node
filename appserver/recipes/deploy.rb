00609139b3ecbc68669406e1dfe0f7420bc2d4dd
Will Button <will@willbutton.co>
2016-09-24 14:31:18 -0700

    trying to stop looking for db:migrate file

--- appserver/recipes/deploy.rb
+++ appserver/recipes/deploy.rb
@@ -2,6 +2,7 @@ deploy 'private_repo' do
   repo 'https://github.com/rekibnikufesin/advanced_pm2.git'
   migrate false
   keep_releases 5
+   symlink_before_migrate({})
   restart_command "cd /srv/www/app/current && npm install && pm2 startOrRestart ecosystem.json"
   user 'root'
   deploy_to '/srv/www/app'
