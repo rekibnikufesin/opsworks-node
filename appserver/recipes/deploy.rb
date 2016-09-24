d1085dbdb756f9b5f9e253fea2102b69f5edcfa1
Will Button <will@willbutton.co>
2016-09-24 14:24:16 -0700

    adding features and fixing db:migrate errror

--- appserver/recipes/deploy.rb
+++ appserver/recipes/deploy.rb
@@ -1,5 +1,8 @@
 deploy 'private_repo' do
   repo 'https://github.com/rekibnikufesin/advanced_pm2.git'
+  migrate false
+  keep_releases 5
+  restart_command "cd /srv/www/app/current && npm install && pm2 startOrRestart ecosystem.json"
   user 'root'
   deploy_to '/srv/www/app'
   action :deploy
