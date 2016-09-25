763beb819abf9a671524b4aebb53cb139ebe72e7
Will Button <will@willbutton.co>
2016-09-24 17:12:36 -0700

    I hate Chef

--- appserver/recipes/deploy.rb
+++ appserver/recipes/deploy.rb
@@ -1,6 +1,13 @@
 app = search("aws_opsworks_app").first
 instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
 layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
+env_var = '"CONTAINER":"unknown"'
+
+if layers.include?("api-layer")
+    env_var = '"CONTAINER":"api"'
+elsif layers.include?("web-layer")
+    env_var = '"CONTAINER":"web"'
+end
 
 directory '/etc/pm2/conf.d' do
   owner 'root'
