d5cce010c0468652e56f777fcabf5b02b2372676
Will Button <will@willbutton.co>
2016-11-03 16:06:39 -0700

    Updating to reflect prod

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -1,18 +1,22 @@
+instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
+layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
+app = search("aws_opsworks_app").first
+env_var = ""
+
 execute 'add nodejs repo' do
   command 'curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -'
 end
 
 yum_package 'nodejs'
-package ['gcc-c++', 'make']
+package ['gcc-c++', 'make', 'openssl-devel']
 yum_package 'ImageMagick'
 
 execute 'install pm2' do
   command 'npm install pm2 -g'
 end
 
-instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
-layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
 
+# setting environment vars for shell access
 if layers.include?("api-layer")
     Chef::Log.info("** setting container to api")
     execute 'add api env var' do
@@ -28,4 +32,98 @@ else
     execute 'add unknown env var' do
       command 'echo CONTAINER="unknown" >> /root/.bashrc && export CONTAINER="unknown"'
     end
+end
+
+# building environment vars
+app['environment'].each do |key,value|
+  env_var = env_var << "\"#{key}\":\"#{value}\","
+end
+
+if layers.include?("api-layer")
+    env_var = env_var + '"CONTAINER":"api"'
+elsif layers.include?("web-layer")
+    env_var = env_var + '"CONTAINER":"web"'
+else
+    env_var = env_var + '"CONTAINER":"unknown"'
+end
+
+directory '/etc/pm2/conf.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+  notifies :create, 'file[/root/.ssh/id_rsa]', :immediately
+  notifies :create, 'directory[/tmp/.ssh]', :immediately
+end
+
+directory '/root/.ssh' do
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end
+
+file '/root/.ssh/id_rsa' do
+  content app['app_source']['ssh_key']
+  owner 'root'
+  group 'root'
+  mode '0600'
+  action :create_if_missing
+  notifies :touch, 'file[/root/.ssh/known_hosts]', :immediately
+end
+
+file '/root/.ssh/known_hosts' do
+  owner 'root'
+  group 'root'
+  action :nothing
+  notifies :run, 'execute[genssh]', :immediately
+end
+
+execute 'genssh' do
+  command "ssh-keygen -R bitbucket.org"
+  action :nothing
+  notifies :run, 'execute[add_known_hosts]', :immediately
+end
+
+execute 'add_known_hosts' do
+  command "ssh-keyscan -H bitbucket.org >> /root/.ssh/known_hosts"
+  action :nothing
+  notifies :create, 'directory[/tmp/.ssh]', :immediately
+end
+
+directory '/tmp/.ssh' do
+  owner 'root'
+  group 'root'
+  mode '0770'
+  recursive true
+  action :nothing
+  notifies :create, 'template[/tmp/.ssh/chef_ssh_deploy_wrapper.sh]', :immediately
+  notifies :create, 'file[/root/.ssh/id_rsa]', :immediately
+end
+
+template "/tmp/.ssh/chef_ssh_deploy_wrapper.sh" do
+  source "chef_ssh_deploy_wrapper.sh.erb"
+  owner 'root'
+  mode 0770
+  action :nothing
+  notifies :create, 'directory[/srv/www/app/log]', :immediately
+end
+
+
+directory '/srv/www/app/log' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+  action :nothing
+  notifies :create, 'directory[/srv/www/app/releases]', :immediately
+end
+
+directory '/srv/www/app/releases' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+  action :nothing
 end
--- appserver/recipes/deploy.rb
+++ appserver/recipes/deploy.rb
@@ -1,22 +1,47 @@
 app = search("aws_opsworks_app").first
 instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
 layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
-env_var = app['environment'].to_s
+release = Time.now.strftime("%Y%m%d%H%M")
+env_var = ""
 
-if layers.include?("api-layer")
-    env_var = env_var + ',"CONTAINER":"api"'
-elsif layers.include?("web-layer")
-    env_var = env_var + ',"CONTAINER":"web"'
-else
-    env_var = env_var + ',"CONTAINER":"unknown"'
+# building environment vars
+app['environment'].each do |key,value|
+  env_var = env_var << "\"#{key}\":\"#{value}\","
 end
 
-directory '/etc/pm2/conf.d' do
+if layers.include?("api-layer")
+    env_var = env_var + '"CONTAINER":"api"'
+elsif layers.include?("web-layer")
+    env_var = env_var + '"CONTAINER":"web"'
+else
+    env_var = env_var + '"CONTAINER":"unknown"'
+end
+
+directory '/tmp/.ssh' do
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0770'
   recursive true
   action :create
+  notifies :create, 'template[/tmp/.ssh/chef_ssh_deploy_wrapper.sh]', :immediately
+end
+
+template "/tmp/.ssh/chef_ssh_deploy_wrapper.sh" do
+  source "chef_ssh_deploy_wrapper.sh.erb"
+  owner 'root'
+  mode 0770
+  action :nothing
+  notifies :sync, "git[/srv/www/app/releases/#{release}]", :immediately
+end
+
+git "/srv/www/app/releases/#{release}" do
+  repository app['app_source']['url']
+  ssh_wrapper "/tmp/.ssh/chef_ssh_deploy_wrapper.sh"
+  revision "master"
+  checkout_branch "master"
+  enable_checkout false
+  action :nothing
+  notifies :create, 'template[/etc/pm2/conf.d/server.json]', :immediately
 end
 
 template '/etc/pm2/conf.d/server.json' do
@@ -25,22 +50,36 @@ template '/etc/pm2/conf.d/server.json' do
   group 'root'
   mode '0644'
   variables :environments => { 'vars' => env_var }
+  action :nothing
+  notifies :run, 'execute[app perms]', :immediately
 end
 
-file '/root/.ssh/id_rsa' do
-  content app['app_source']['ssh_key']
-  owner 'root'
-  group 'root'
-  mode '0600'
+execute 'app perms' do
+  command "chown -R root:root /srv/www/app/releases/#{release}"
+  action :nothing
+  notifies :run, 'execute[set file perms]', :immediately
 end
 
-deploy 'private_repo' do
-  repo app['app_source']['url']
-  migrate false
-  keep_releases 5
-  symlink_before_migrate({})
-  restart_command "cd /srv/www/app/current && npm install && pm2 startOrRestart /etc/pm2/conf.d/server.json"
-  user 'root'
-  deploy_to '/srv/www/app'
-  action :deploy
+execute 'set file perms' do
+  command "setfacl -Rdm g:root:rwx /srv/www/app/releases/#{release}"
+  action :nothing
+  notifies :run, 'execute[npm install]', :immediately
+end
+
+execute 'npm install' do
+  command "su - root -c 'cd /srv/www/app/releases/#{release} && npm install'"
+  action :nothing
+  notifies :create, 'link[/srv/www/app/current]', :immediately
+end
+
+link '/srv/www/app/current' do
+  to "/srv/www/app/releases/#{release}"
+  link_type :symbolic
+  action :nothing
+  notifies :run, 'execute[pm2]', :immediately
+end
+
+execute 'pm2' do
+  command "pm2 startOrRestart /etc/pm2/conf.d/server.json"
+  action :nothing
 end
